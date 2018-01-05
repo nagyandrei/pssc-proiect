d6e3e3e88d17f1d26e7d0e7d9b7710fa67adbecb
nagyandrei <nagyandrei95@gmail.com>
2018-01-05 19:27:02 +0200

    Added commands

---
+++ SQLQuery1.sql
@@ -0,0 +1,1 @@
+﻿select * from System_Users;
---
+++ proiect pssc/Model/Masina/StareMasina.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proiect_pssc.Model.Masina
+{
+    public enum StareMasina
+    {
+        InStoc,
+        StocInsuficient,
+        Rezervata,
+        Vanduta
+    }
+}
